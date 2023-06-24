ac984e6ce7d180cb766112795a7240330b665b51
EgoTheReactDev <eego12745@gmail.com>
2023-06-24 16:33:05 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import SearchUser from './components/SearchUser';
+import UserDetails from './components/UserDetails';
+import RepositoryList from './components/RepositoryList';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
   return (
     <div>
-
+      <SearchUser setUser={setUser} />
+      {user && (
+        <div>
+          <UserDetails user={user} />
+          <RepositoryList repositories={user.repositories} />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
