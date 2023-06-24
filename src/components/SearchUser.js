3a830deb6590da501fec8289529329b42fd5e9d1
EgoTheReactDev <eego12745@gmail.com>
2023-06-24 16:27:31 +0100

    app

---
+++ src/components/SearchUser.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const SearchUser = ({ setUser }) => {
+  const [username, setUsername] = useState("");
+
+  return <div></div>;
+};
+
+export default SearchUser;
