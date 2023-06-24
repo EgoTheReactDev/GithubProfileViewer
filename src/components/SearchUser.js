f4ee456d4edea3cd278fba274a553702778fb620
EgoTheReactDev <eego12745@gmail.com>
2023-06-24 16:28:11 +0100

    searchuser

--- src/components/SearchUser.js
+++ src/components/SearchUser.js
@@ -4,6 +4,10 @@ import axios from "axios";
 const SearchUser = ({ setUser }) => {
   const [username, setUsername] = useState("");
 
+  const handleInputChange = (event) => {
+    setUsername(event.target.value);
+  };
+
   return <div></div>;
 };
 
