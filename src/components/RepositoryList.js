442482d1354666d3cb4d50d8c3888e1e27054b7b
EgoTheReactDev <eego12745@gmail.com>
2023-06-24 17:00:26 +0100

    repo

--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const RepositoryList = ({ repositories }) => {
+const RepositoryList = ({ user }) => {
+  const { repositoriesData } = user;
+
   return (
     <div className="repository-list">
       <h3>Repositories:</h3>
       <ul>
-        {repositories.map((repo) => (
+        {repositoriesData.map((repo) => (
           <li key={repo.id}>
             <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
               {repo.name}
--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const UserDetails = ({ user }) => {
+  const { userData } = user;
+
   return (
-    <div>
-      <h2>{user.login}</h2>
-      <img src={user.avatar_url} alt={user.login} />
-      <p>{user.bio}</p>
-      <p>Followers: {user.followers}</p>
+    <div className="user-card">
+      <img src={userData.avatar_url} alt={userData.login} />
+      <h2>{userData.name}</h2>
+      <p>{userData.bio}</p>
+      <p>Followers: {userData.followers}</p>
+      <p>Following: {userData.following}</p>
     </div>
   );
 };
