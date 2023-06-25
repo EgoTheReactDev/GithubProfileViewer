1033b017b5af08bee5124838048c0d38d7d12d94
EgoTheReactDev <eego12745@gmail.com>
2023-06-25 12:20:28 +0100

    css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
-import SearchUser from './components/SearchUser';
-import UserDetails from './components/UserDetails';
-import RepositoryList from './components/RepositoryList';
+import React, { useState } from "react";
+import SearchUser from "./components/SearchUser";
+import UserDetails from "./components/UserDetails";
+import RepositoryList from "./components/RepositoryList";
 
 const App = () => {
   const [user, setUser] = useState(null);
 
   return (
-    <div>
-      <SearchUser setUser={setUser} />
-      {user && (
-        <div>
-          <UserDetails user={user} />
-          <RepositoryList user={user} />
-        </div>
-      )}
+    <div className="mainBody">
+      <div className="left">
+        <SearchUser setUser={setUser} />
+      </div>
+      <div className="right">
+        {user && (
+          <div className="">
+            <UserDetails user={user} />
+            <RepositoryList user={user} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
